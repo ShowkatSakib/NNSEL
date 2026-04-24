83b06a6beb643edfb9fe4af02d080c045fb05176
Takbir Zaman Bhuiyan <takbirzamanbhuiyan@gmail.com>
2026-04-25 00:09:22 +0600

    new folder added

---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import About from '../components/About/About'; // Make sure the path matches your filename
+
+function Home() {
+  return (
+    <div>
+    
+      <About /> 
+    </div>
+  );
+}
+
+export default Home;
