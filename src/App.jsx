e54b4eb0cedbe6c484cf3504e19d4d67c8fb8915
Takbir Zaman Bhuiyan <takbirzamanbhuiyan@gmail.com>
2026-04-28 12:49:41 +0600

    final &  fresh code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// We've updated the path to look inside the "pages" folder!
+import Home from "./pages/Home"; 
+
 import About from "./components/About";
 import Contact from "./components/Contact";
+import InteriorDesignPage from "./components/InteriorDesignPage";
 
 export default function App() {
   return (
-    <>
-      <About />
-      <Contact />
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* Your full scrolling landing page */}
+        <Route path="/" element={<Home />} />
+        
+        {/* Your standalone routes */}
+        <Route path="/about" element={<About />} />
+      
+        <Route path="/interior-design" element={<InteriorDesignPage />} />
+  <Route path="/contact" element={<Contact />} />
+
+      </Routes>
+    </Router>
+  );
 }
